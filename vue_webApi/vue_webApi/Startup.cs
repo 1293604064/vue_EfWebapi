da226a2806c744412c0fe53ff05e0cb1066db6a7
wangjin <1293604064@qq.com>
2020-05-15 00:20:51 +0800

    修改ef

--- vue_webApi/vue_webApi/Startup.cs
+++ vue_webApi/vue_webApi/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using vue_webApi.Entities;
 
 namespace vue_webApi
 {
@@ -25,6 +26,7 @@ namespace vue_webApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSingleton<AskquestionsContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
