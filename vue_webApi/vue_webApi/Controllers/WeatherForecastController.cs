845932c1efd7d29fdfdcb96b1754a1ae590354bd
wangjin <1293604064@qq.com>
2020-05-15 00:30:56 +0800

    修改控制器中的方法

--- vue_webApi/vue_webApi/Controllers/WeatherForecastController.cs
+++ vue_webApi/vue_webApi/Controllers/WeatherForecastController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using vue_webApi.Entities;
 
 namespace vue_webApi.Controllers
 {
@@ -15,25 +16,20 @@ namespace vue_webApi.Controllers
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
-
+        
         private readonly ILogger<WeatherForecastController> _logger;
+        private readonly AskquestionsContext _context;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, AskquestionsContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public IEnumerable<User> Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return _context.User;
         }
     }
 }
